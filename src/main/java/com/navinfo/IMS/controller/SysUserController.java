9bfdec10f9644cfefde6845c5c9514cb9071aacd
AbnerL <603273557@qq.com>
2017-11-15 11:43:46 +0800

    登录功能修改

--- src/main/java/com/navinfo/IMS/controller/SysUserController.java
+++ src/main/java/com/navinfo/IMS/controller/SysUserController.java
@@ -5,6 +5,9 @@ import com.github.pagehelper.PageInfo;
 import com.navinfo.IMS.dto.Msg;
 import com.navinfo.IMS.entity.SysUser;
 import com.navinfo.IMS.service.SysUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
@@ -22,15 +25,28 @@ public class SysUserController {
     @Autowired
     private SysUserService sysUserService;
 
+    /**
+     * 登录
+     * @param username
+     * @param password
+     * @return
+     */
     @RequestMapping(value = "/toLogin" ,method = RequestMethod.POST)
     public String login(String username,String password){
-        if(!StringUtils.isEmpty(username)){
-            boolean flag=sysUserService.getSysUser(username,password);
-            if (flag){
-                return "redirect:index";
-            }
+        Subject subject= SecurityUtils.getSubject();
+        UsernamePasswordToken token=new UsernamePasswordToken(username,password);
+        String redirectUrl="redirect:login";
+        try{
+            subject.login(token);
+            redirectUrl= "redirect:index";
+        }catch(UnknownAccountException e){
+            System.out.println("对用户["+username+"]验证未通过,未知账户");
+        }catch(IncorrectCredentialsException e){
+            System.out.println("对用户["+username+"]验证未通过,凭证错误");
+        }catch (LockedAccountException e){
+            System.out.println("对用户["+username+"]验证未通过,账户锁定");
         }
-        return "login";
+        return redirectUrl;
     }
 
     /**
@@ -72,9 +88,9 @@ public class SysUserController {
     @ResponseBody
     @RequestMapping(value="/user/{id}",method=RequestMethod.GET )
     public Msg getUser(@PathVariable("id") String userId){
-        List<SysUser> users=sysUserService.getUserById(userId);
-        if(users!=null&&users.size()!=0){
-            return Msg.success().add("users",users);
+        SysUser user=sysUserService.getUserById(userId);
+        if(user!=null){
+            return Msg.success().add("users",user);
         }else{
             return Msg.failure();
         }
