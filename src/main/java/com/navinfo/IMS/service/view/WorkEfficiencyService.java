f7b0edd67d925cce374acc16dd5e276e712b52e6
AbnerL <603273557@qq.com>
2017-12-17 20:51:57 +0800

    修改

---
+++ src/main/java/com/navinfo/IMS/service/view/WorkEfficiencyService.java
@@ -0,0 +1,11 @@
+package com.navinfo.IMS.service.view;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by luozhihui on 2017/12/3.
+ */
+public interface WorkEfficiencyService {
+    List findWorkEfficiency(Map map);
+}
