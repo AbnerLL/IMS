2d5c6750194940c4edfe7368df9563ef8e520cbc
AbnerL <603273557@qq.com>
2018-01-28 18:12:50 +0800

    增加根据用户初始化菜单

---
+++ src/main/java/com/navinfo/core/vo/ModuleVO.java
@@ -0,0 +1,24 @@
+package com.navinfo.core.vo;
+
+import com.navinfo.core.entity.SysModule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 菜单模块的数据展示类
+ * Created by luozhihui on 2018/1/22.
+ */
+public class ModuleVO extends SysModule{
+
+    //子模块对象
+    private List<SysModule> children=new ArrayList<SysModule>();
+
+    public List<SysModule> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<SysModule> children) {
+        this.children = children;
+    }
+}
